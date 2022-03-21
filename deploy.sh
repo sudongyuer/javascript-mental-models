12447f6f29d29a65fe78c975ae02d73dc47e5a25
sudongyu <976499226@qq.com>
2022-03-21 10:25:38 +0800

    chore: update config

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:sudongyuer/javascript-mental-models.git main:gh-pages
 
 cd -
