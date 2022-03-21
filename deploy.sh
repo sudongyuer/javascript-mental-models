503f33ca6c8ca0eed7befddcd3cacd832084018f
sudongyu <976499226@qq.com>
2022-03-21 11:03:55 +0800

    chore: update

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:sudongyuer/javascript-mental-models.git main:gh-pages
+ git push -f git@github.com:sudongyuer/javascript-mental-models.git master:gh-pages-deploy
+
+ cd ../../../ && rm -rf docs/.vitepress/dist
 
 cd -
